cb47dde1828648336a6cf97f634d2924aa067434
Mike Wilcox <mwilcox56@gmail.com>
2014-12-20 19:59:35 -0500

    new up instances, cleanup

--- lib/js/actions/ruscello-server-action-creators.ts
+++ lib/js/actions/ruscello-server-action-creators.ts
@@ -1,16 +1,18 @@
 import RuscelloDispatcher = require('../dispatcher/ruscello-dispatcher');
 import RuscelloConstants = require('../constants/ruscello-constants');
 
-var ActionTypes = RuscelloConstants.ActionTypes;
-
-export interface IRuscelloServerActionCreators {
+interface IRuscelloServerActionCreators {
   receiveTweet(any): void;
 }
 
 class RuscelloServerActionCreators implements IRuscelloServerActionCreators {
 
   receiveTweet(tweet) {
-    RuscelloDispatcher.handleServerAction({
+
+    var ActionTypes = new RuscelloConstants().ActionTypes();
+    var dispatcher = new RuscelloDispatcher();
+
+    dispatcher.handleServerAction({
       type: ActionTypes.RECEIVE_RAW_TWEET,
       rawTweet: tweet
     });
