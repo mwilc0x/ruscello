67de67e634ea750b589f984938141ee1de67c8b0
Mike Wilcox <mwilcox56@gmail.com>
2014-12-20 19:09:49 -0500

    correct the typescript

--- lib/js/actions/ruscello-server-action-creators.ts
+++ lib/js/actions/ruscello-server-action-creators.ts
@@ -3,9 +3,13 @@ import RuscelloConstants = require('../constants/ruscello-constants');
 
 var ActionTypes = RuscelloConstants.ActionTypes;
 
-class RuscelloServerActionCreators {
+export interface IRuscelloServerActionCreators {
+  receiveTweet(any): void;
+}
 
-  receiveTweet: function(tweet) {
+class RuscelloServerActionCreators implements IRuscelloServerActionCreators {
+
+  receiveTweet(tweet) {
     RuscelloDispatcher.handleServerAction({
       type: ActionTypes.RECEIVE_RAW_TWEET,
       rawTweet: tweet
