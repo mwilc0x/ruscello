b583ec84492a37f85f152f3a5a4f185c630b7b3d
Mike Wilcox <mwilcox56@gmail.com>
2014-12-20 19:09:07 -0500

    correct the typescript

--- lib/js/dispatcher/ruscello-dispatcher.ts
+++ lib/js/dispatcher/ruscello-dispatcher.ts
@@ -3,25 +3,30 @@
 import RuscelloConstants = require('../constants/ruscello-constants');
 import flux = require('flux');
 
-class RuscelloDispatcher extends flux.Dispatcher<any> {
+export interface IRuscelloDispatcher extends flux.Dispatcher<any> {
+  handleServerAction(any): void;
+  handleViewAction(any): void;
+}
+
+class RuscelloDispatcher implements IRuscelloDispatcher {
 
   /**
    * @param {object} action The details of the action, including the action's
    * type and additional data coming from the server.
    */
-  handleServerAction: function(action) {
+  handleServerAction(action) {
     var payload = {
       source: RuscelloConstants.PayloadSources.SERVER_ACTION,
       action: action
     };
     this.dispatch(payload);
-  },
+  }
 
   /**
    * @param {object} action The details of the action, including the action's
    * type and additional data coming from the view.
    */
-  handleViewAction: function(action) {
+  handleViewAction(action) {
     var payload = {
       source: RuscelloConstants.PayloadSources.VIEW_ACTION,
       action: action
@@ -29,6 +34,6 @@ class RuscelloDispatcher extends flux.Dispatcher<any> {
     this.dispatch(payload);
   }
 
-});
+}
 
 export = RuscelloDispatcher;
