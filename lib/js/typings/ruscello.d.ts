9ce8aa6557c12ba52bdc67b41adade2284e7a018
Mike Wilcox <mwilcox56@gmail.com>
2014-12-24 07:52:16 -0500

    updated typings

--- lib/js/typings/ruscello.d.ts
+++ lib/js/typings/ruscello.d.ts
@@ -1,12 +1,12 @@
 interface RuscelloModel {
-  tweet: Tweet;
+  book: Book;
   payloadSources: PayloadSources;
   actionTypes: ActionTypes;
 }
 
-interface Tweet { id: string; text: string; }
+interface Book { id: string; index: string; summary: string; }
 interface PayloadSources { SERVER_ACTION: string; VIEW_ACTION: string; }
-interface ActionTypes { RECEIVE_RAW_TWEET: string; }
+interface ActionTypes { RECEIVE_BOOKS: string; }
 
 declare module 'ruscello.model' {
   var rm: RuscelloModel;
