176b6a16cfe2a2f70f24c24b193fb53cc5d9ebdb
Mike Wilcox <mwilcox56@gmail.com>
2014-12-26 16:10:26 -0500

    add support for tsd

---
+++ typings/tsd.d.ts
@@ -0,0 +1,14 @@
+
+/// <reference path="cheerio/cheerio.d.ts" />
+/// <reference path="eventemitter2/eventemitter2.d.ts" />
+/// <reference path="express/express.d.ts" />
+/// <reference path="form-data/form-data.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="q/Q.d.ts" />
+/// <reference path="react/legacy/react-addons-0.11.d.ts" />
+/// <reference path="react/react.d.ts" />
+/// <reference path="request/request.d.ts" />
+/// <reference path="flux/flux.d.ts" />
+/// <reference path="socket.io-client/socket.io-client.d.ts" />
+/// <reference path="socket.io/socket.io.d.ts" />
+/// <reference path="../node_modules/typed-react/dist/typed-react.d.ts" />
