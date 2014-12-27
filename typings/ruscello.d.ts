02b840ae1a75882a7c9802903039cf1ab8aac0b8
Mike Wilcox <mwilcox56@gmail.com>
2014-12-26 19:09:43 -0500

    add local type definitions

---
+++ typings/ruscello.d.ts
@@ -0,0 +1,18 @@
+/// <reference path='../lib/js/components/book-bestseller.react.ts' />
+/// <reference path='../lib/js/components/book-bestseller-list.react.ts' />
+/// <reference path='../node_modules/typed-react/dist/typed-react.d.ts' />
+
+interface RuscelloModel {
+  book: Book;
+  payloadSources: PayloadSources;
+  actionTypes: ActionTypes;
+}
+
+interface Book { id: string; index: string; summary: string; }
+interface PayloadSources { SERVER_ACTION: string; VIEW_ACTION: string; }
+interface ActionTypes { RECEIVE_BOOKS: string; }
+
+declare module 'ruscello.model' {
+  var rm: RuscelloModel;
+  export = rm;
+}
